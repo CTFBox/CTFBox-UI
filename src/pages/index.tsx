a61ef80197fae6748f1b0c1fb1c0ad617d7280c6
hota1024 <hotalog@hota1024.com>
2020-11-21 21:11:29 +0900

    🏷️ Setup typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  )
+}
+
+export default HomePage
