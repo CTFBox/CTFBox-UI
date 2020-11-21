bbbd77101501078d33128c2ba94e36679e2914e5
hota1024 <hotalog@hota1024.com>
2020-11-21 21:18:03 +0900

    💄 Wrap home with MainLayout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { NextPage } from 'next'
+import { MainLayout } from '../layouts/MainLayout'
 
 /**
  * HomePage component.
  */
 export const HomePage: NextPage = () => {
   return (
-    <>
+    <MainLayout>
       <h1>Home</h1>
-    </>
+    </MainLayout>
   )
 }
 
