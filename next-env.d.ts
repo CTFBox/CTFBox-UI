ec17baee36e470e616ee056aba77ab824419778c
hota1024 <hotalog@hota1024.com>
2020-11-22 23:53:16 +0900

    🏷️ Add styled-components theme types

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+import 'styled-components'
+import { Theme } from './src/types/Theme'
+
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
