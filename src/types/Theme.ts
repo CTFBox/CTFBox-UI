b1d568f288ab1e0656d945694967739d9638125b
hota1024 <hotalog@hota1024.com>
2020-11-22 22:06:45 +0900

    🏷️ Add Theme type

---
+++ src/types/Theme.ts
@@ -0,0 +1,17 @@
+/**
+ * Theme type.
+ */
+export type Theme = {
+  primary: string
+  secondary: string
+  success: string
+  error: string
+  black: string
+  blackLight: string
+  blackBlue: string
+  white: string
+  background: string
+  text: string
+  textGray: string
+  textWhite: string
+}
