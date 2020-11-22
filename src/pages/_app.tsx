e0444dfc869a44ce08981ed87be0b4884f8f691b
hota1024 <hotalog@hota1024.com>
2020-11-22 22:07:32 +0900

    💄 Add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,37 @@
 import { AppProps } from 'next/app'
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Theme } from '../types/Theme'
 
 export default function App({
   Component,
   pageProps,
 }: AppProps): React.ReactElement {
+  const [theme] = useState<Theme>({
+    primary: '#FFC683',
+    secondary: '#83C3FF',
+    success: '#A7E891',
+    error: '#FF8383',
+    black: '#000000',
+    blackLight: '#202020',
+    blackBlue: '#14171B',
+    white: '#FFFFFF',
+    background: '#F5F5F5',
+    text: '#202020',
+    textGray: '#B0B0B0',
+    textWhite: '#FFFFFF',
+  })
+
   return (
     <>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
 
       <style global jsx>{`
         html,
         body {
+          background: ${theme.background};
           padding: 0;
           margin: 0;
         }
