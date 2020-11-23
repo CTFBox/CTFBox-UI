aeba9fbf3a7a427a3b2c73a236cf125cd43c7b3d
hota1024 <hotalog@hota1024.com>
2020-11-23 16:41:38 +0900

    💄 Add header and main container

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,31 @@
+import styled from 'styled-components'
+import { Navbar } from '../components/Navbar'
+
+const Main = styled.main`
+  display: flex;
+  flex: 1;
+
+  height: calc(100vh - 64px);
+`
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1600px;
+  margin: 0 auto;
+`
+
 /**
  * Main layout.
  */
 export const MainLayout: React.FC = (props) => {
   return (
     <>
-      <div>{props.children}</div>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <Main>
+        <Container>{props.children}</Container>
+      </Main>
     </>
   )
 }
