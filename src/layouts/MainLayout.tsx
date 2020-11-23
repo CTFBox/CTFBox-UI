8443b0270b363e540875aa116f34b645cacb386e
hota1024 <hotalog@hota1024.com>
2020-11-23 16:51:29 +0900

    💄 Add container padding

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,15 +2,17 @@ import styled from 'styled-components'
 import { Navbar } from '../components/Navbar'
 
 const Main = styled.main`
+  min-height: calc(100vh - 64px);
+
   display: flex;
   flex: 1;
-
-  height: calc(100vh - 64px);
 `
 
 const Container = styled.div`
   width: 100%;
   max-width: 1600px;
+
+  padding: 0 16px;
   margin: 0 auto;
 `
 
