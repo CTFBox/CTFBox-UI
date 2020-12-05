1005e012b50d969dab283431b78fb8d09c7f861d
hota1024 <hotalog@hota1024.com>
2020-12-05 16:57:20 +0900

    🏷️ Fix index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,56 @@
 import { NextPage } from 'next'
+import { useState } from 'react'
+import { GridContainer } from '../components/GridContainer'
+import { PageTitle } from '../components/PageTitle'
+import { QuestionCard } from '../components/QuestionCard'
 import { MainLayout } from '../layouts/MainLayout'
+import { Question } from '../types/Question'
 
 /**
  * HomePage component.
  */
 export const HomePage: NextPage = () => {
+  const [questions] = useState<Question[]>([
+    {
+      questionID: 1001,
+      title: 'Start of CTFBox',
+      detail: 'Welcome to CTFBox!',
+    },
+    {
+      questionID: 1002,
+      title: 'Start of CTFBox',
+      detail: 'Welcome to CTFBox!',
+    },
+    {
+      questionID: 1003,
+      title: 'Start of CTFBox',
+      detail: 'Welcome to CTFBox!',
+    },
+    {
+      questionID: 1004,
+      title: 'Start of CTFBox',
+      detail: 'Welcome to CTFBox!',
+    },
+    {
+      questionID: 1005,
+      title: 'Start of CTFBox',
+      detail: 'Welcome to CTFBox!',
+    },
+    {
+      questionID: 1006,
+      title: 'Start of CTFBox',
+      detail: 'Welcome to CTFBox!',
+    },
+  ])
+
   return (
     <MainLayout>
-      <h1>Home</h1>
+      <PageTitle>Problems</PageTitle>
+      <GridContainer>
+        {questions.map((q) => (
+          <QuestionCard key={q.questionID} question={q}></QuestionCard>
+        ))}
+      </GridContainer>
     </MainLayout>
   )
 }
