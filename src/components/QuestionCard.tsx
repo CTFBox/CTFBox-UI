8d464d153b61a0b44c103d48b1fd88a2fb741876
hota1024 <hotalog@hota1024.com>
2020-12-05 16:33:40 +0900

    💄 Fix QuestionCard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,8 @@
-import styled from 'styled-components'
 import { Question } from '../types/Question'
-import { Card, CardText, CardTitle } from './Card'
+import { Button } from './Button'
+import { Card, CardActions, CardText, CardTitle } from './Card'
 import { Divider } from './Divider'
+import { Link } from './Link'
 
 /**
  * QuestionCardProps type.
@@ -21,6 +22,12 @@ export const QuestionCard: React.FC<QuestionCardProps> = (props) => {
       <CardTitle>{question.title}</CardTitle>
       <Divider />
       <CardText>{question.detail}</CardText>
+      <Divider />
+      <CardActions>
+        <Link to={`/${question.questionID}`}>
+          <Button>Try</Button>
+        </Link>
+      </CardActions>
     </Card>
   )
 }
